f4beb11a56e8eeeb8f2a529271d92237d86c1486
Nicolas Dabène <ndabene2807@gmail.com>
2025-09-04 16:15:21 +0200

    feat(order): add updated range filter

--- src/ApiPlatform/Resources/Order/OrderList.php
+++ src/ApiPlatform/Resources/Order/OrderList.php
@@ -22,10 +22,10 @@ declare(strict_types=1);
 
 namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Order;
 
-use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
-use PrestaShopBundle\ApiPlatform\Metadata\PaginatedList;
+use ApiPlatform\Metadata\ApiResource;
 use PrestaShop\Module\APIResources\ApiPlatform\Resources\Order\State\OrderProvider;
+use PrestaShopBundle\ApiPlatform\Metadata\PaginatedList;
 use Symfony\Component\HttpFoundation\Response;
 
 #[ApiResource(
@@ -36,6 +36,56 @@ use Symfony\Component\HttpFoundation\Response;
             scopes: ['order_read'],
             openapiContext: [
                 'summary' => 'List orders',
+                'parameters' => [
+                    [
+                        'name' => 'date_from',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'string',
+                        ],
+                    ],
+                    [
+                        'name' => 'date_to',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'string',
+                        ],
+                    ],
+                    [
+                        'name' => 'updated_from',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'string',
+                        ],
+                    ],
+                    [
+                        'name' => 'updated_to',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'string',
+                        ],
+                    ],
+                    [
+                        'name' => 'status_id',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'int',
+                        ],
+                    ],
+                    [
+                        'name' => 'q',
+                        'in' => 'query',
+                        'required' => false,
+                        'schema' => [
+                            'type' => 'string',
+                        ],
+                    ],
+                ],
             ],
         ),
         new PaginatedList(
@@ -92,5 +142,3 @@ class OrderList
     /** @var string ISO 8601 */
     public string $dateAdd;
 }
-
-
