118c5924cc0e124573b352ec55f501302f81b93c
Progi1984 <franck@lefevre.dev>
2025-09-29 08:44:02 +0200

    Added endpoints for domain "Theme"

---
+++ src/ApiPlatform/Resources/Theme/ThemeImport.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Theme;
+
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Theme\Command\ImportThemeCommand;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSUpdate;
+
+#[ApiResource(
+    operations: [
+        new CQRSUpdate(
+            uriTemplate: '/themes/import',
+            output: false,
+            extraProperties: [
+                // ImportThemeCommand only accepts its ValueObject directly in its constructor, which the
+                // generic CQRSApiNormalizer cannot build before this core fix.
+                'minVersion' => '9.2.0',
+            ],
+            CQRSCommand: ImportThemeCommand::class,
+            CQRSCommandMapping: [
+                '[importSource]' => '[importSource]',
+            ],
+            normalizationContext: [
+                'skip_null_values' => false,
+            ],
+        ),
+    ],
+)]
+class ThemeImport
+{
+    public array $importSource;
+}
