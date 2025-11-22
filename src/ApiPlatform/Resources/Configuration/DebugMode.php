fa2c5815eb2e8bb6bcd5301b239ecf4e6f84af6b
Arnaud <arnaud.scote@gmail.com>
2025-11-22 11:40:15 +0100

    Api admin route : SwitchDebugMode

---
+++ src/ApiPlatform/Resources/Configuration/DebugMode.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Configuration;
+
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Configuration\Command\SwitchDebugModeCommand;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSUpdate;
+
+#[ApiResource(
+    operations: [
+/*        new CQRSGet(
+            uriTemplate: '/debug/status',
+            CQRSQuery: GetDebugModeStatus::class,
+            scopes: ['debug_mode_read']
+        ),*/
+        new CQRSUpdate(
+            uriTemplate: '/debug/toggle',
+            CQRSCommand: SwitchDebugModeCommand::class,
+            scopes: ['debug_mode_write']
+        ),
+    ],
+)]
+class DebugMode
+{
+    public string $enableDebugMode;
+}
