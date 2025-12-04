d601211ebb68e7c649f11d2b4113e89ba4d98feb
Sebas <sebastian.jaimovich@prestashop.com>
2025-12-04 14:26:34 +0100

    refactor combination-list test and start working on combionation test refactoring

--- tests/Integration/ApiPlatform/ProductCombinationListEndpointTest.php
+++ tests/Integration/ApiPlatform/ProductCombinationListEndpointTest.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace PsApiResourcesTest\Integration\ApiPlatform;
 
+use Tests\Resources\Resetter\LanguageResetter;
 use Tests\Resources\Resetter\ProductResetter;
-use Tests\Resources\ResourceResetter;
 
 class ProductCombinationListEndpointTest extends ApiTestCase
 {
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
-        (new ResourceResetter())->backupTestModules();
+        // Add the fr-FR language to test multi lang values accurately
+        LanguageResetter::resetLanguages();
         ProductResetter::resetProducts();
+        self::addLanguageByLocale('fr-FR');
+        // Pre-create the API Client with the needed scopes, this way we reduce the number of created API Clients
         self::createApiClient(['product_read']);
     }
 
@@ -21,7 +24,7 @@ class ProductCombinationListEndpointTest extends ApiTestCase
     {
         parent::tearDownAfterClass();
         ProductResetter::resetProducts();
-        (new ResourceResetter())->resetTestModules();
+        LanguageResetter::resetLanguages();
     }
 
     public static function getProtectedEndpoints(): iterable
@@ -32,57 +35,17 @@ class ProductCombinationListEndpointTest extends ApiTestCase
         ];
     }
 
-    public function testGetProductCombinations(): void
+    /**
+     * @return int
+     */
+    public function testGetProductCombinationList(): int
     {
-        // 1. Create Product
-        $product = new \Product();
-        $product->name = ['en-US' => 'Test Product', 'fr-FR' => 'Produit Test'];
-        $product->link_rewrite = ['en-US' => 'test-product', 'fr-FR' => 'produit-test'];
-        $product->price = 10.0;
-        $product->active = true;
-        $product->id_category_default = 2; // Home
-        $product->add();
-        $productId = (int) $product->id;
-
-        // 2. Create Attribute Group
-        $attributeGroup = new \AttributeGroup();
-        $attributeGroup->name = [\Configuration::get('PS_LANG_DEFAULT') => 'Size'];
-        $attributeGroup->public_name = [\Configuration::get('PS_LANG_DEFAULT') => 'Size'];
-        $attributeGroup->group_type = 'select';
-        $attributeGroup->add();
-        $attributeGroupId = (int) $attributeGroup->id;
-
-        // 3. Create Attribute
-        $attribute = new \Attribute();
-        $attribute->id_attribute_group = $attributeGroupId;
-        $attribute->name = [\Configuration::get('PS_LANG_DEFAULT') => 'Small'];
-        $attribute->add();
-        $attributeId = (int) $attribute->id;
-
-        // 4. Create Combination
-        $combination = new \Combination();
-        $combination->id_product = $productId;
-        $combination->reference = 'TEST-COMB-1';
-        $combination->price = 5.0;
-        $combination->quantity = 10;
-        $combination->minimal_quantity = 1;
-        $combination->add();
-        $combinationId = (int) $combination->id;
-
-        // Link attribute to combination
-        $combination->setAttributes([$attributeId]);
-
-        // 5. Call API
-        $response = $this->getItem('/products/' . $productId . '/combinations', ['product_read']);
-
-        // 6. Assert
-        $this->assertCount(1, $response['items']);
-        $this->assertEquals($combinationId, $response['items'][0]['combinationId']);
-        $this->assertEquals($productId, $response['items'][0]['productId']);
-
-        // Clean up
-        $product->delete();
-        $attributeGroup->delete();
-        // Attribute and Combination should be deleted by cascade or we rely on resetter
+        $productId = 1;
+        $productCombinationList = $this->getItem('/products/' . $productId . '/combinations', ['product_read']);
+        $combinationIdExpected = 1;
+        $this->assertIsArray($productCombinationList);
+        $this->assertContains($combinationIdExpected, $productCombinationList);
+        return $productId;
     }
+
 }
