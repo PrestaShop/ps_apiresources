e742901869ceb5ee99715be954e8c11936416ab1
luigi massa <lmassa@bwlab.it>
2026-03-09 22:21:19 +0100

    Implement GET /carriers/{carrierId}/ranges API endpoint

---
+++ src/ApiPlatform/Resources/Carrier/CarrierRanges.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Carrier;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\Exception\CarrierNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\Query\GetCarrierRanges;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSGet;
+use Symfony\Component\HttpFoundation\Response;
+
+#[ApiResource(
+    operations: [
+        new CQRSGet(
+            uriTemplate: '/carriers/{carrierId}/ranges',
+            requirements: ['carrierId' => '\d+'],
+            CQRSQuery: GetCarrierRanges::class,
+            scopes: ['carrier_read'],
+            CQRSQueryMapping: self::QUERY_MAPPING,
+        ),
+    ],
+    normalizationContext: ['skip_null_values' => false],
+    exceptionToStatus: [
+        CarrierNotFoundException::class => Response::HTTP_NOT_FOUND,
+    ],
+)]
+class CarrierRanges
+{
+    #[ApiProperty(identifier: true)]
+    public int $carrierId;
+
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'array',
+            'items' => [
+                'type' => 'object',
+                'properties' => [
+                    'zoneId' => ['type' => 'integer'],
+                    'ranges' => [
+                        'type' => 'array',
+                        'items' => [
+                            'type' => 'object',
+                            'properties' => [
+                                'from' => ['type' => 'string'],
+                                'to' => ['type' => 'string'],
+                                'price' => ['type' => 'string'],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]
+    )]
+    public array $zones = [];
+
+    public const QUERY_MAPPING = [
+        '[carrierId]' => '[getCarrierId]',
+        '[zones]' => '[getZones]',
+    ];
+}
