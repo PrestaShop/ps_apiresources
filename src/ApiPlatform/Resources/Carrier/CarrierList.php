60e3ac8888b1ead39ef615c14c4fd4fd69e55318
luigi massa <lmassa@bwlab.it>
2026-03-09 22:17:02 +0100

    Add GET /carriers API endpoint

---
+++ src/ApiPlatform/Resources/Carrier/CarrierList.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Carrier;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\Exception\CarrierNotFoundException;
+use PrestaShop\PrestaShop\Core\Search\Filters\CarrierFilters;
+use PrestaShopBundle\ApiPlatform\Metadata\PaginatedList;
+use PrestaShopBundle\ApiPlatform\Provider\QueryListProvider;
+use Symfony\Component\HttpFoundation\Response;
+
+#[ApiResource(
+    operations: [
+        new PaginatedList(
+            uriTemplate: '/carriers',
+            provider: QueryListProvider::class,
+            scopes: ['carrier_read'],
+            ApiResourceMapping: [
+                '[id_carrier]' => '[carrierId]',
+                '[is_free]' => '[isFree]',
+            ],
+            gridDataFactory: 'prestashop.core.grid.data.factory.carrier_decorator',
+            filtersClass: CarrierFilters::class,
+        ),
+    ],
+    exceptionToStatus: [
+        CarrierNotFoundException::class => Response::HTTP_NOT_FOUND,
+    ],
+)]
+class CarrierList
+{
+    #[ApiProperty(identifier: true)]
+    public int $carrierId;
+
+    public string $name;
+
+    public ?string $delay;
+
+    public bool $active;
+
+    public bool $isFree;
+
+    public int $position;
+
+    public ?string $logo;
+}
