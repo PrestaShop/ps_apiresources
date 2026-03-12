e6a573846c98bacd0aa60ff671c2ef4356e64ffc
Pichat <morgan.pichat@prestashop.com>
2026-03-12 14:27:42 +0100

    search-engine-delete

---
+++ src/ApiPlatform/Resources/SearchEngine/BulkDeleteSearchEngines.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\SearchAlias;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Alias\Command\BulkDeleteSearchTermsAliasesCommand;
+use PrestaShop\PrestaShop\Core\Domain\Alias\Exception\AliasNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\SearchEngine\Command\BulkDeleteSearchEngineCommand;
+use PrestaShop\PrestaShop\Core\Domain\SearchEngine\Exception\DeleteSearchEngineException;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSDelete;
+use Symfony\Component\HttpFoundation\Response;
+
+#[ApiResource(
+    operations: [
+        new CQRSDelete(
+            uriTemplate: '/search-engines/bulk-delete',
+            CQRSCommand: BulkDeleteSearchEngineCommand::class,
+            scopes: ['search_engine_write'],
+            CQRSCommandMapping: [
+                '[searchEngineIds]' => '[searchEngineIds]',
+            ],
+            allowEmptyBody: false,
+            experimentalOperation: true,
+        ),
+    ],
+    exceptionToStatus: [
+        DeleteSearchEngineException::class => Response::HTTP_NOT_FOUND,
+    ],
+)]
+class BulkDeleteSearchEngines
+{
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'array',
+            'items' => ['type' => 'int'],
+        ]
+    )]
+    public array $searchEngineIds = [];
+}
