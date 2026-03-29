d11553d03c28b09ca07a8884fa8c15c568df85fd
pcescon <pcescon@degetel.com>
2026-03-29 23:30:36 +0200

    feat: add additional Product API endpoints
    Add 24 new Product endpoints:
    - DELETE/PUT /products/bulk-delete, bulk-update-status
    - POST /products/{id}/duplicate - duplicate product
    - PUT/DELETE /products/{id}/categories - manage categories
    - GET/POST/PATCH/DELETE /specific-prices - specific price CRUD
    - PUT /products/{id}/stock - update stock
    - PUT/DELETE /products/{id}/related-products - manage related
    - PUT/DELETE /products/{id}/tags - manage tags
    - PUT /products/{id}/carriers - set carriers
    - PATCH/DELETE /combinations/{id} - update/delete combination
    - DELETE /combinations/bulk-delete - bulk delete combinations
    - POST /products/{id}/virtual-product-file - add virtual file
    - PUT/DELETE /virtual-product-files/{id} - manage virtual files
    - PUT/DELETE /products/{id}/features - manage features

---
+++ src/ApiPlatform/Resources/Product/CombinationUpdate.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @author    Pascal Cescon <pascal.cescon@gmail.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Product;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Command\DeleteCombinationCommand;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Command\UpdateCombinationCommand;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Exception\CannotDeleteCombinationException;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Exception\CannotUpdateCombinationException;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Exception\CombinationNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Query\GetCombinationForEditing;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSDelete;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSPartialUpdate;
+use PrestaShopBundle\ApiPlatform\Metadata\LocalizedValue;
+use Symfony\Component\HttpFoundation\Response;
+
+#[ApiResource(
+    operations: [
+        new CQRSPartialUpdate(
+            uriTemplate: '/combinations/{combinationId}',
+            requirements: ['combinationId' => '\d+'],
+            CQRSCommand: UpdateCombinationCommand::class,
+            CQRSQuery: GetCombinationForEditing::class,
+            scopes: ['product_write'],
+        ),
+        new CQRSDelete(
+            uriTemplate: '/combinations/{combinationId}',
+            requirements: ['combinationId' => '\d+'],
+            CQRSCommand: DeleteCombinationCommand::class,
+            scopes: ['product_write'],
+        ),
+    ],
+    exceptionToStatus: [
+        CombinationNotFoundException::class => Response::HTTP_NOT_FOUND,
+        CannotUpdateCombinationException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+        CannotDeleteCombinationException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+    ],
+)]
+class CombinationUpdate
+{
+    #[ApiProperty(identifier: true)]
+    public int $combinationId;
+
+    public ?bool $isDefault = null;
+
+    public ?string $gtin = null;
+
+    public ?string $isbn = null;
+
+    public ?string $mpn = null;
+
+    public ?string $reference = null;
+
+    public ?string $upc = null;
+
+    public ?string $impactOnWeight = null;
+
+    public ?string $impactOnPrice = null;
+
+    public ?string $ecoTax = null;
+
+    public ?string $impactOnUnitPrice = null;
+
+    public ?string $wholesalePrice = null;
+
+    public ?int $minimalQuantity = null;
+
+    public ?int $lowStockThreshold = null;
+
+    public ?string $availableDate = null;
+
+    #[LocalizedValue]
+    public ?array $localizedAvailableNowLabels = null;
+
+    #[LocalizedValue]
+    public ?array $localizedAvailableLaterLabels = null;
+}
---
+++ src/ApiPlatform/Resources/Product/SpecificPrice.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @author    Pascal Cescon <pascal.cescon@gmail.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\Product;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Command\AddSpecificPriceCommand;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Command\DeleteSpecificPriceCommand;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Command\EditSpecificPriceCommand;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Exception\CannotAddSpecificPriceException;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Exception\CannotDeleteSpecificPriceException;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Exception\CannotUpdateSpecificPriceException;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Exception\SpecificPriceConstraintException;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Exception\SpecificPriceNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Product\SpecificPrice\Query\GetSpecificPriceForEditing;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSCreate;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSDelete;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSGet;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSPartialUpdate;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(
+    operations: [
+        new CQRSGet(
+            uriTemplate: '/specific-prices/{specificPriceId}',
+            requirements: ['specificPriceId' => '\d+'],
+            CQRSQuery: GetSpecificPriceForEditing::class,
+            scopes: ['product_read'],
+        ),
+        new CQRSCreate(
+            uriTemplate: '/specific-prices',
+            CQRSCommand: AddSpecificPriceCommand::class,
+            CQRSQuery: GetSpecificPriceForEditing::class,
+            scopes: ['product_write'],
+        ),
+        new CQRSPartialUpdate(
+            uriTemplate: '/specific-prices/{specificPriceId}',
+            requirements: ['specificPriceId' => '\d+'],
+            CQRSCommand: EditSpecificPriceCommand::class,
+            CQRSQuery: GetSpecificPriceForEditing::class,
+            scopes: ['product_write'],
+        ),
+        new CQRSDelete(
+            uriTemplate: '/specific-prices/{specificPriceId}',
+            requirements: ['specificPriceId' => '\d+'],
+            CQRSCommand: DeleteSpecificPriceCommand::class,
+            scopes: ['product_write'],
+        ),
+    ],
+    exceptionToStatus: [
+        SpecificPriceNotFoundException::class => Response::HTTP_NOT_FOUND,
+        SpecificPriceConstraintException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+        CannotAddSpecificPriceException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+        CannotUpdateSpecificPriceException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+        CannotDeleteSpecificPriceException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+    ],
+)]
+class SpecificPrice
+{
+    #[ApiProperty(identifier: true)]
+    public int $specificPriceId;
+
+    #[Assert\NotBlank]
+    public int $productId;
+
+    #[Assert\NotBlank]
+    #[Assert\Choice(choices: ['amount', 'percentage'])]
+    public string $reductionType;
+
+    #[Assert\NotBlank]
+    public string $reductionValue;
+
+    public bool $includeTax = true;
+
+    public string $fixedPrice = '-1';
+
+    public int $fromQuantity = 1;
+
+    public ?string $dateTimeFrom = null;
+
+    public ?string $dateTimeTo = null;
+
+    public ?int $shopId = null;
+
+    public ?int $combinationId = null;
+
+    public ?int $currencyId = null;
+
+    public ?int $countryId = null;
+
+    public ?int $groupId = null;
+
+    public ?int $customerId = null;
+}
