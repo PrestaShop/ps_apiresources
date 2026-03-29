6b8349a5b1bd5ea5ed31f528651f35e76f3405ee
pcescon <pcescon@degetel.com>
2026-03-29 22:37:42 +0200

    feat: add CatalogPriceRule API endpoints
    Add API resources for CatalogPriceRule domain:
    - GET/POST/PATCH/DELETE /catalog-price-rules/{id}
    - DELETE /catalog-price-rules/bulk-delete
    
    5 new endpoints for managing catalog price rules with reduction support.

---
+++ src/ApiPlatform/Resources/CatalogPriceRule/BulkCatalogPriceRules.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @author    Pascal Cescon <pascal.cescon@gmail.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources\CatalogPriceRule;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Command\BulkDeleteCatalogPriceRuleCommand;
+use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\CannotDeleteCatalogPriceRuleException;
+use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\CatalogPriceRuleNotFoundException;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSDelete;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(
+    operations: [
+        new CQRSDelete(
+            uriTemplate: '/catalog-price-rules/bulk-delete',
+            CQRSCommand: BulkDeleteCatalogPriceRuleCommand::class,
+            scopes: ['catalog_price_rule_write'],
+        ),
+    ],
+    exceptionToStatus: [
+        CatalogPriceRuleNotFoundException::class => Response::HTTP_NOT_FOUND,
+        CannotDeleteCatalogPriceRuleException::class => Response::HTTP_UNPROCESSABLE_ENTITY,
+    ],
+)]
+class BulkCatalogPriceRules
+{
+    /**
+     * @var int[]
+     */
+    #[ApiProperty(openapiContext: ['type' => 'array', 'items' => ['type' => 'integer'], 'example' => [1, 3]])]
+    #[Assert\NotBlank]
+    public array $catalogPriceRuleIds;
+}
