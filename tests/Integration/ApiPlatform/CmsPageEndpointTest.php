1746d94779a2df414c3813bb2c1dfd0627bd5c4c
axel-paillaud <contact@axelweb.fr>
2026-03-29 14:36:36 +0200

    add test for CmsPage endpoint

---
+++ tests/Integration/ApiPlatform/CmsPageEndpointTest.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PsApiResourcesTest\Integration\ApiPlatform;
+
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Resources\DatabaseDump;
+use Tests\Resources\Resetter\LanguageResetter;
+
+class CmsPageEndpointTest extends ApiTestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        LanguageResetter::resetLanguages();
+        self::addLanguageByLocale('fr-FR');
+        self::resetTables();
+        self::createApiClient(['cms_page_read', 'cms_page_write']);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+        LanguageResetter::resetLanguages();
+        self::resetTables();
+    }
+
+    protected static function resetTables(): void
+    {
+        DatabaseDump::restoreTables([
+            'cms',
+            'cms_lang',
+            'cms_shop',
+        ]);
+    }
+
+    public static function getProtectedEndpoints(): iterable
+    {
+        yield 'get endpoint' => ['GET', '/cms-pages/1'];
+        yield 'create endpoint' => ['POST', '/cms-pages'];
+    }
+
+    public function testAddCmsPage(): int
+    {
+        $postData = [
+            'cmsPageCategoryId' => 1,
+            'titles' => [
+                'en-US' => 'Test CMS Page',
+                'fr-FR' => 'Page CMS de test',
+            ],
+            'metaTitles' => [
+                'en-US' => 'Meta Title EN',
+                'fr-FR' => 'Meta Title FR',
+            ],
+            'metaDescriptions' => [
+                'en-US' => 'Meta description EN',
+                'fr-FR' => 'Meta description FR',
+            ],
+            'friendlyUrls' => [
+                'en-US' => 'test-cms-page',
+                'fr-FR' => 'page-cms-de-test',
+            ],
+            'contents' => [
+                'en-US' => '<p>Content EN</p>',
+                'fr-FR' => '<p>Contenu FR</p>',
+            ],
+            'indexedForSearch' => true,
+            'enabled' => true,
+            'shopIds' => [1],
+        ];
+
+        $response = $this->createItem('/cms-pages', $postData, ['cms_page_write']);
+        $this->assertArrayHasKey('cmsPageId', $response);
+        $cmsPageId = $response['cmsPageId'];
+
+        $this->assertEquals(
+            ['cmsPageId' => $cmsPageId] + $postData,
+            $response
+        );
+
+        return $cmsPageId;
+    }
+
+    /** @depends testAddCmsPage */
+    public function testGetCmsPage(int $cmsPageId): int
+    {
+        $response = $this->getItem('/cms-pages/' . $cmsPageId, ['cms_page_read']);
+        $this->assertEquals([
+            'cmsPageId' => $cmsPageId,
+            'cmsPageCategoryId' => 1,
+            'titles' => [
+                'en-US' => 'Test CMS Page',
+                'fr-FR' => 'Page CMS de test',
+            ],
+            'metaTitles' => [
+                'en-US' => 'Meta Title EN',
+                'fr-FR' => 'Meta Title FR',
+            ],
+            'metaDescriptions' => [
+                'en-US' => 'Meta description EN',
+                'fr-FR' => 'Meta description FR',
+            ],
+            'friendlyUrls' => [
+                'en-US' => 'test-cms-page',
+                'fr-FR' => 'page-cms-de-test',
+            ],
+            'contents' => [
+                'en-US' => '<p>Content EN</p>',
+                'fr-FR' => '<p>Contenu FR</p>',
+            ],
+            'indexedForSearch' => true,
+            'enabled' => true,
+            'shopIds' => [1],
+        ], $response);
+
+        return $cmsPageId;
+    }
+
+    public function testInvalidCmsPage(): void
+    {
+        // Missing required titles (default language) and friendlyUrls
+        $invalidData = [
+            'cmsPageCategoryId' => 1,
+            'titles' => [
+                // en-US (default language) is missing
+                'fr-FR' => 'Page CMS<',
+            ],
+            'friendlyUrls' => [
+                // en-US (default language) is missing
+                'fr-FR' => 'invalid friendly url with spaces',
+            ],
+            'metaTitles' => [],
+            'metaDescriptions' => [],
+            'contents' => [],
+            'indexedForSearch' => true,
+            'enabled' => true,
+            'shopIds' => [1],
+        ];
+
+        $response = $this->createItem(
+            '/cms-pages',
+            $invalidData,
+            ['cms_page_write'],
+            Response::HTTP_UNPROCESSABLE_ENTITY
+        );
+
+        $this->assertValidationErrors([
+            [
+                'propertyPath' => 'titles',
+                'message' => 'The field titles is required at least in your default language.',
+            ],
+            [
+                'propertyPath' => 'titles[fr-FR]',
+                'message' => '"Page CMS<" is invalid',
+            ],
+            [
+                'propertyPath' => 'friendlyUrls',
+                'message' => 'The field friendlyUrls is required at least in your default language.',
+            ],
+            [
+                'propertyPath' => 'friendlyUrls[fr-FR]',
+                'message' => '"invalid friendly url with spaces" is invalid',
+            ],
+        ], $response);
+    }
+}
